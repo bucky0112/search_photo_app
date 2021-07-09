111ec8d78f562e7097d715509cb95fbf5fda441c
bucky0112 <bucky0112@gmail.com>
2021-07-09 23:00:03 +0800

    app can get state from searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import SearchBar from './SearchBar'
 
 function App() {
+  const onSearchSubmit = (input) => {
+    console.log(input)
+  }
+
   return (
     <div className='ui container' style={{ marginTop: 20 }}>
-      <SearchBar />
+      <SearchBar handleSubmit={onSearchSubmit} />
     </div>
   )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const { handleSubmit } = props
+  const [state, setState] = useState({ inputText: '' })
+
+  const handleInput = (e) => {
+    setState({ inputText: e.target.value })
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    handleSubmit(state.inputText)
+  }
+
   return (
     <div className='ui segment'>
-      <form className='ui form'>
+      <form className='ui form' onSubmit={handleFormSubmit}>
         <div className='field'>
           <label htmlFor='search'>Search</label>
-          <input id='search' type='text' />
+          <input
+            id='search'
+            type='text'
+            value={state.inputText}
+            onChange={handleInput}
+          />
         </div>
       </form>
     </div>
